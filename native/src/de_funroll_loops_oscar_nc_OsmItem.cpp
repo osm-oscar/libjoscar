7fb4bb799080ae8cf1d49d41db6f604bf37732ff
Daniel Bahrdt <daniel.git@funroll-loops.de>
2016-09-14 14:32:36 +0200

    fix OsmItem::ancestors() which returned wrong results for items that are regions. For items that are regions the GeoHierarchy has to be used to get the ancestors

--- native/src/de_funroll_loops_oscar_nc_OsmItem.cpp
+++ native/src/de_funroll_loops_oscar_nc_OsmItem.cpp
@@ -77,11 +77,26 @@ JNIEXPORT jintArray JNICALL Java_de_funroll_1loops_oscar_nc_OsmItem_ancestors
 	try {
 		auto itemPtr = objStore.get(id);
 		const auto & gh = itemPtr->db().geoHierarchy();
-		auto cells = itemPtr->cells();
 		std::vector<int> ancestors;
-		for(uint32_t c : cells) {
-			for(uint32_t i(gh.cellParentsBegin(c)), s(gh.cellParentsEnd(c)); i < s; ++i) {
-				ancestors.emplace_back( gh.cellPtrs().at(i) );
+
+		if (itemPtr->isRegion()) {
+			auto region = gh.region( gh.storeIdToGhId( itemPtr->id() ) );
+			for(uint32_t i(0), s(region.parentsSize()); i < s; ++i) {
+				ancestors.emplace_back( region.parent(i) );
+			}
+			for(std::size_t i(0); i < ancestors.size(); ++i) {
+				region = gh.region(ancestors.at(i));
+				for(uint32_t j(0), s(region.parentsSize()); j < s; ++j) {
+					ancestors.emplace_back( region.parent(j) );
+				}
+			}
+		}
+		else {
+			auto cells = itemPtr->cells();
+			for(uint32_t c : cells) {
+				for(uint32_t i(gh.cellParentsBegin(c)), s(gh.cellParentsEnd(c)); i < s; ++i) {
+					ancestors.emplace_back( gh.cellPtrs().at(i) );
+				}
 			}
 		}
 		using std::sort;
