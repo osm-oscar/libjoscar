f4e8bba5ff8e0439c1954fd3949dc32791cfc51a
Daniel Bahrdt <daniel.git@funroll-loops.de>
2017-02-20 18:36:40 +0100

    OsmKeyValueObjectStore: buffer size

--- java/src/de/funroll_loops/oscar/OsmKeyValueObjectStore.java
+++ java/src/de/funroll_loops/oscar/OsmKeyValueObjectStore.java
@@ -1,12 +1,14 @@
 package de.funroll_loops.oscar;
 
 public class OsmKeyValueObjectStore extends Base<de.funroll_loops.oscar.nc.OsmKeyValueObjectStore> {
+	private int m_size = 0;
 	
 	public OsmKeyValueObjectStore() {
 	}
 
 	public OsmKeyValueObjectStore(int ptr) {
 		super(new de.funroll_loops.oscar.nc.OsmKeyValueObjectStore(), ptr);
+		m_size = cnc().size( ptr() );
 	}
 
 	public int numberOfRegions() {
@@ -14,7 +16,7 @@ public class OsmKeyValueObjectStore extends Base<de.funroll_loops.oscar.nc.OsmKe
 	}
 
 	public int size() {
-		return cnc().size( ptr() );
+		return m_size;
 	}
 	
 	public OsmItem at(int pos) {
